4d1991e6e3ec2ba924984038a22c7041c9f91c2d
Trey Kemp <kemptrey@outlook.com>
2021-04-15 13:58:56 -0600

    init

--- App.js
+++ App.js
@@ -1,12 +1,85 @@
-import React from 'react'
-import {View, Text} from 'react-native'
-
+// react has philosphy learn once write anywhere
+import axios from 'axios'
+import useAxios from 'axios-hooks'
+import React, { useState, useEffect } from 'react'
+import { View, Button, Text, SafeAreaView, StyleSheet, ScrollView, Image, TouchableOpacity, ActivityIndicator } from 'react-native'
+import CardDemo from './components/CardDemo'
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 const App = () => {
+  const [{ data, loading, error }, refetch] = useAxios(
+    'https://reqres.in/api/users?delay=3'
+  )
+  const renderData = ()=>{
+    return data.data.map(person=>{
+      return <CardDemo key={person.id} {...person}/>
+    })
+  }
+  const getContent = () => {
+    console.log(data)
+    if (loading) return    <ActivityIndicator size="large" color="#0000ff" />
+    if (error) return <Text>Error</Text>
+    else {
+      return (
+        <>
+          <View style={styles.header} >
+            <Text style={styles.headerText}>List of Things</Text>
+          </View>
+          <ScrollView>
+            {renderData()}
+          </ScrollView>
+        </>
+      )
+    }
+  }
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <NavigationContainer>
+      <SafeAreaView style={styles.appContainer}>
+        {getContent()}
+      </SafeAreaView>
+    </NavigationContainer>
   )
 }
-
-export default App 
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+  touchable: {
+    borderColor: 'red',
+    borderWidth: 1
+  },
+  img: {
+    width: 20,
+    height: 20
+    // flex:1
+  },
+  header: {
+    padding: 20,
+    borderColor: "black",
+    borderBottomWidth: 3
+  },
+  headerText: {
+    fontSize: 20,
+    fontWeight: "700"
+  },
+  cardContainer: {
+    borderColor: "#bbb",
+    borderWidth: 2,
+    marginHorizontal: 10,
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 10
+  },
+  cardHeader: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  }
+})
+// const styles = {
+//   container: { 
+//     border:'2px solid'
+//   }
+// }
+export default App
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-/**
- * @format
- */
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+export default function App() {
+  return (
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
+AppRegistry.registerComponent(appName, () => (
+  <NavigationContainer>
+    <App/>
+  </NavigationContainer>
+)
 
-AppRegistry.registerComponent(appName, () => App);
